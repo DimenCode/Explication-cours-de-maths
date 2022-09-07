694d16d42290673532db2d1209fff8c6c5e69519
DimenCode <81313341+DimenCode@users.noreply.github.com>
2022-09-07 19:16:10 +0200

    Update explications.py

--- explications.py
+++ explications.py
@@ -1,7 +1,10 @@
 # explications de base
 
+# chaîne de caractère dite "str"
 laVariable = "ici, c'est du texte"
+# suite de chiffres "int"
 variable = 2022
+
 l_autre_variable = 14.3
 
 # là ça va fonctionner
@@ -12,3 +15,5 @@ print(variable)
 
 # mais là oui
 print(str(variable))
+
+# pour transformer un nombre en texte, il faut faire int(variable)
