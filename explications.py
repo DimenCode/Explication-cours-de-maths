90a6faef4c327dcd3256ee9eeb95188b994189d6
DimenCode <81313341+DimenCode@users.noreply.github.com>
2022-09-07 19:12:20 +0200

    Create explications.py

---
+++ explications.py
@@ -0,0 +1,14 @@
+# explications de base
+
+laVariable = "ici, c'est du texte"
+variable = 2022
+l_autre_variable = 14.3
+
+# là ça va fonctionner
+print(laVariable)
+
+# ici non
+print(variable)
+
+# mais là oui
+print(str(variable))
